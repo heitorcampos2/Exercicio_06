6c89db3a697e2709460b6c7b2fd62851713bf6c7
heitorcampos2 <heitormtb12@gmail.com>
2019-05-14 20:19:50 -0300

    v001

---
+++ src/Escola/TestaPessoa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Escola;
+
+/**
+ *
+ * @author ifnmg
+ */
+public class TestaPessoa {
+        public static void main(String[] args){
+        
+        Pessoa a1 = new Pessoa("","","");
+        //a1.setNome("Larissa");
+        //a1.setMatricula("007");
+        
+        System.out.println("Nome: "+a1.getNome());
+        System.out.println("Matricula: "+a1.getEndereco());
+        System.out.println("Matricula: "+a1.getTelefone());
+        
+      
+        
+    }
+}
